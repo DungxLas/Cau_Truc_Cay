0307bbc8ea3e8a8100603758b08e56f67067879d
DungxLas <phamjhungfdungx@gmail.com>
2022-06-05 19:52:13 +0700

    Initial Commit

---
+++ Cau_Truc_Cay/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cau_Truc_Cay
+//
+//  Created by PHẠM HÙNG DŨNG on 05/06/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
